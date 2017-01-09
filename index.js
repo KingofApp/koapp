387fd565e0d1360d7acd19079e7ce9a5a6aa0cb0
totegsito <pacoquesmun@gmail.com>
2017-01-09 11:38:17 +0100

    koapp generators tools

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+  parseCategories     : parseCategories,
+  fixPluginName       : fixPluginName,
+  camelize            : camelize,
+  fixPluginCategories : fixPluginCategories
+}
+
+/**
+* @param {String} user's input
+*/
+function parseCategories(input) {
+  this.options.categories = input.split(',');
+}
+
+/** Function that validate the Spinner name
+* @returns {String}
+*/
+function fixPluginName(name, ReplaceSymbol) {
+  name = name.toLowerCase().trim();
+  return name.replace(/ /g, ReplaceSymbol);
+}
+
+/** Function that validate the Categories
+* @returns {String}
+*/
+function fixPluginCategories(list) {
+  list = list.replace(/ /g, '').toLowerCase().trim();
+  var arrayCategories = list.split(',');
+  return JSON.stringify(arrayCategories);
+}
+
+/** Function that converts a string into camel case using javascript regex
+ * @author Christian C. Salvadó
+ * @see http://stackoverflow.com/questions/2970525/converting-any-string-into-camel-case
+ * @param {string} user's input
+ * @returns {string} camelCase
+*/
+function camelize(str) {
+  return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function (letter, index) {
+    return index === 0 ? letter.toLowerCase() : letter.toUpperCase();
+  }).replace(/\s+/g, '');
+}
